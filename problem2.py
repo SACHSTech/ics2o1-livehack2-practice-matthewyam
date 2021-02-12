9d71a8d7495b13ea80f13edff9909d80f076943b
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2021-02-12 15:30:25 +0000

    Completed question 2

--- problem2.py
+++ problem2.py
@@ -1,1 +1,15 @@
 
+print("What will I be doing during the summer")
+print(" ")
+
+mark = int(input("What your average?(Number only): "))
+earnings_before_the_summer = int(input("How much money have you saved?: $"))
+
+if mark >= 80 and earnings_before_the_summer >= 500:
+  print("You can go to Europe!"")
+
+elif mark >= 80  and earnings_before_the_summer < 500:
+  print("You can go to California")
+
+else:
+  print("You cannot go anywhere")
